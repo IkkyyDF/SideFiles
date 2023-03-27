4c7b9b56df073fc3edfc512061fe53e129e9f101
IkkyyDF <daffmks@gmail.com>
2023-03-27 21:49:08 +0800

    New

---
+++ DataGames.lua
@@ -0,0 +1,137 @@
+return {
+    Shindo = {
+        Id = {
+            4616652839,
+            4601350214,
+            4601350394,
+            4601350656,
+            4601350809,
+            5431071837,
+            5447073001,
+            5084678830,
+            5431069982,
+            4601350760,
+            6341670805,
+            6505734854,
+            6901575446,
+            6984568732,
+            6986372023,
+            5451398863,
+            7214033433,
+            5451401540,
+            5307141034,
+            7534339269,
+            5451410109,
+            6593187011,
+            6593188260,
+            5743370338,
+            5664805984,
+            5664803952,
+            8184506020,
+            8472733618,
+            7923764447,
+            9310522814,
+            5451405681,
+            7524809704,
+            7524811367,
+            6602058266,
+            5255237254,
+            6602103757,
+            5824792748,
+            4616652839,
+            4601350214,
+            4601350394,
+            4601350656,
+            4601350809,
+            5431071837,
+            5447073001,
+            5431069982,
+            4601350760,
+            6341670805,
+            6505734854,
+            6901575446,
+            6984568732,
+            6986372023,
+            5451398863,
+            7214033433,
+            5451401540,
+            5307141034,
+            7534339269,
+            5451410109,
+            6593187011,
+            6593188260,
+            5743370338,
+            5664805984,
+            5664803952,
+            8184506020,
+            8472733618,
+            7923764447,
+            9310522814,
+            5451405681,
+            7524809704,
+            7524811367,
+            6602058266,
+            5255237254,
+            6602103757,
+            5824792748
+        },
+        Data = {
+            Maintenance = false,
+            Reason = "Im Too Tired, Ill Try To Fix It Again Tomorrow, 6 Hours Fixing Still Cant Fix it :(",
+            Name = "Shindo Life",
+            Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/ShindoLife.lua"
+        }
+    },
+    Sfs = {
+        Id = 11040063484,
+        Data = {
+            Maintenance = false,
+            Reason = "Fixing Bug!",
+            Name = "Fix A Little Bug!!!!!",
+            Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/SwordFightersSimulator.lua"
+        }
+    },
+    asou = {
+        Id = 11542692507,
+        Data = {
+            Maintenance = false,
+            Reason = "Fix A Little Bug!!!!!",
+            Name = "Anime Soul Simulator",
+            Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/AnimeSoulSimulator.lua"
+        }
+    },
+    ads = {
+        Id = {
+            6938803436,
+            7338881230,
+            6990129309,
+            6990133340,
+            7274690025,
+            6990131029
+        },
+        Data = {
+            Maintenance = false,
+            Reason = "Fix A Little Bug!!!!!",
+            Name = "Anime Dimension Sim",
+            Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/AnimeDimensionsSimulator.lua"
+        }
+    },
+    rc = {
+        Id = 9285238704,
+        Data = {
+            Maintenance = false,
+            Reason = "",
+            Name = "Race Clicker",
+            Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/RaceClicker.lua"
+        }
+    },
+    fw = {
+        Id = 12192552089,
+        Data = {
+            Maintenance = false,
+            Reason = "Coming Soon",
+            Name = "FruitWarriors",
+            Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/FruitWarriors.lua"
+        }
+    }
+}
---
+++ Version.lua
@@ -0,0 +1,12 @@
+local ver = {
+    Shindo = "v.6.5",
+    Pnw = "v.2.0",
+    Sfs = "v.2.5",
+    asou = "v.1.0",
+    ads = "v.1.5",
+    ofs = "v.2.0",
+    rc = "v.1.0",
+    fw = "v.1.0",
+    bf = "v.1.0"
+}
+return ver
