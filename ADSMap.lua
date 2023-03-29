e5bb53c77cfb58f40767ba3fbfeb393d63a55139
IkkyyDF <daffmks@gmail.com>
2023-03-29 23:39:47 +0800

    Create ADSMap.lua

---
+++ ADSMap.lua
@@ -0,0 +1,23 @@
+local map = {
+    Dimension = {
+        "Infinite Mode-Level 20+",
+        "Titan Dimension-Level 1+",
+        "Demon Dimension-Level 16+",
+        "Curse Dimension-Level 31+",
+        "Villain Dimension-Level 46+",
+        "Sword Dimension-Level 61+",
+        "Ghoul Dimension-Level 76+",
+        "Fate Dimension-Level 91+",
+        "Slime Dimension-Level 106+",
+        "Devil Dimension-Level 121+", 
+        "Pirate Dimension-Level 131+"
+    },
+    Raid = {
+        "Gear 5 Fluffy Raid-Level 15+", 
+        "Tengoku Raid-Level 15+", 
+        "Hirito Raid-Level 15+", 
+        "Titan Raid -Level 15+"
+    }
+}
+
+return map
