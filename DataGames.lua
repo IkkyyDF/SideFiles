6ddea4acdc9dd29981d2c22f3d4e3a819d0c60a6
IkkyyDF <daffmks@gmail.com>
2023-07-18 12:08:06 +0800

    Update DataGames.lua

--- DataGames.lua
+++ DataGames.lua
@@ -99,7 +99,7 @@ return {
             Maintenance = false,
             Reason = "Updating!!",
             Name = "Anime Soul Simulator",
-            Ver = "v.4.5",
+            Ver = "v.5.0",
             Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/AnimeSoulSimulator.lua"
         }
     },
