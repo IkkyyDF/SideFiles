eab3c72f6bbc059cf69e83d63b1781c3ead30bd7
IkkyyDF <125168689+IkkyyDF@users.noreply.github.com>
2023-07-24 20:13:39 +0800

    Update PandaAuthCore.lua

--- PandaAuthCore.lua
+++ PandaAuthCore.lua
@@ -1,1 +1,1 @@
-local PandaAuth = loadstring(game:HttpGet(('https://pandadevelopment.net/service_api/PandaBetaLib.lua')))()
+loadstring(game:HttpGet(('https://pandadevelopment.net/service_api/PandaBetaLib.lua')))()
