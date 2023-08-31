3cf0e98c7f3b1d45465d26239058846f68789b86
IkkyyDF <daffmks@gmail.com>
2023-08-31 19:17:56 +0800

    Update DataGames.lua

--- DataGames.lua
+++ DataGames.lua
@@ -149,7 +149,7 @@ return {
             Maintenance = false,
             Reason = "Updating!!",
             Name = "Anime Fighting Simulator X",
-            Ver = "v.3.5",
+            Ver = "v.4.0",
             Link = "https://raw.githubusercontent.com/IkkyyDF/PublicGames/main/AnimeFightingSimulatorX.lua"
         }
     }
